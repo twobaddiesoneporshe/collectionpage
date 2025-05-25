b52e1560336dc0454980996724d940645134fed3
Rafa <rafafitrina@gmail.com>
2025-05-26 01:00:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
-st.title("Rina's collection page")
+st.title("Rina's Collection Page")
 st.write("mostly stan skz and xdiz!!")
 st.image("IMG_0350.jpeg") 
 st.image("IMG_0353.jpeg")
 st.image("IMG_0354.jpeg")
+st.image("IMG_0365.jpeg")
