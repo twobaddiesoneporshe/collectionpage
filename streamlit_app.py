691adbfcf09c8e094d3621c3b4cdd2b7cb470c3c
Rafa <rafafitrina@gmail.com>
2025-05-26 00:37:27 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Rina's collection page")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "mostly stan skz and xdiz!!"
 )
