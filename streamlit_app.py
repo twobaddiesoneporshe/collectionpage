6eb0e55b445d8092f2ff4d0237af151758ebb1c6
Rafa <rafafitrina@gmail.com>
2025-05-27 11:31:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,10 @@ import streamlit as st
 st.title("Rina's Collection Page 🩷")
 st.write("mostly stan skz and xdiz!! ⋆. 𐙚 ˚❀⋆.ೃ࿔*:･")
 st.write("Rafa Fitrina, Kelas X-C")
+
 st.image("IMG_0350.jpeg") 
+st.write("skz maxident era")
+
 st.image("IMG_0353.jpeg")
 st.image("IMG_0354.jpeg")
 st.image("IMG_0365.jpeg") 
